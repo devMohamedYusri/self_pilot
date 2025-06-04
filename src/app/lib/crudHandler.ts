7d0f19a3f720323dafd107e20e7e1c3d7bb109ae
Mohamed Yusri <mohamedyusri205@gmail.com>
2025-06-04 20:35:13 +0300

    removed user unused variable

--- src/app/lib/crudHandler.ts
+++ src/app/lib/crudHandler.ts
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/lib/auth'
 import { prisma } from '@/app/lib/prisma'
 import type { Prisma } from '@prisma/client'
-import type { Task, Goal, Habit, Journal, User } from '@/app/types'
+import type { Task, Goal, Habit, Journal } from '@/app/types'
 
 // Define the models that support CRUD operations
 type CrudModel = 'task' | 'goal' | 'habit' | 'journal'
